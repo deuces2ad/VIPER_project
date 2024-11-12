3795c95496fd5e5d17da9335f9a890a0a062e62e
Abhishek Dhiman <abhishekdhiman1408@gmail.com>
2024-11-12 17:25:03 +0530

    Initial Commit

---
+++ TemplateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TemplateApp
+//
+//  Created by Abhishek Dhiman on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TemplateApp/TemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateAppApp.swift
+//  TemplateApp
+//
+//  Created by Abhishek Dhiman on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
